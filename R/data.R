aa9259e2a4ef69c1d515c7cf7c43ffe613813213
AlannaCollier <alanna.collier.2022@mumail.ie>
2024-12-19 13:00:21 +0000

    Makes the dublin_airport row names into a variable called Month for making our methods

--- R/data.R
+++ R/data.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(janitor)
+library(tibble)
 
 dublin_airport <- read.csv("https://data.gov.ie/dataset/af51e908-b723-4126-9929-004b48d79b80/resource/5f9771a4-657e-44d6-9f96-8816691dd5fc/download/dublinairport_climate_averages_1981_2010.csv",
                            header = FALSE)
@@ -29,6 +30,7 @@ colnames(transposed_data)[2] <- "Mean Daily Sunshine (hours)"
 colnames(transposed_data)[3] <- "Mean Rainfall"
 
 
+
 head(transposed_data)
 
 transposed_data <- transposed_data[-1, ]
@@ -39,6 +41,9 @@ dublin_airport$`Mean Temperature` <- as.numeric(dublin_airport$`Mean Temperature
 dublin_airport$`Mean Daily Sunshine (hours)` <- as.numeric(dublin_airport$`Mean Daily Sunshine (hours)`)
 dublin_airport$`Mean Rainfall` <- as.numeric(dublin_airport$`Mean Rainfall`)
 
+dublin_airport <- dublin_airport[1:12, ]
+
+dublin_airport <- rownames_to_column(dublin_airport, var = "Month")
 
 dublinairport <- function(data) {
   if (!is.data.frame(data)) {
@@ -51,4 +56,5 @@ dublinairport <- function(data) {
 dublin_airport <- dublinairport(dublin_airport)
 
 usethis::use_data(dublin_airport, compress = "xz", overwrite = TRUE)
+
 
