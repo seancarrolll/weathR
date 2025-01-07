fe0e403d420b98a85e856d6a3cbe6627cac10ad6
Roisin1m <roisinmurphy2015@gmail.com>
2025-01-07 22:31:24 +0000

    Added the Roxygen to summarise.dublinairport

--- R/summarise.dublinairport.R
+++ R/summarise.dublinairport.R
@@ -1,3 +1,26 @@
+#' Summarise Dublin Airport Weather Data
+#'
+#'Summarises numeric columns in the `dublinairport` dataset. Allows optional filtering
+#' of rows based on a column and value. Calculates a correlation matrix if specified.
+#'
+#' @param object A dataset of the class "dublin airport".
+#' @param filter_var A character string specifying the name of the column to filter by.
+#'  If 'NULL', no filtering occurs. The default is \code{NULL}.
+#' @param filter_value A numeric value that filters rows where \code{filter_var} is greater
+#'  than or equal to this value. The default is \code{NULL}.
+#' @param summary_func A function to calculate summary statistics for the numeric columns.
+#'  The default is \code{mean}.
+#' @param correlation A logical indicating whether to include the correlation matrix in the output. Defaults to \code{TRUE}.
+#' @param ... Additional arguments used by \code{summary_func}.
+#'
+#' @returns A list containing:
+#'  \item {summary}{A tibble of the summary statistic for the numeric columns.}
+#'  \item {correlation_matrix}{A correlation matrix of the numeric columns (if 'correlation = TRUE').}
+#' @export
+#'
+#' @examples
+#'
+#'
 summarise.dublinairport <- function(object, filter_var = NULL, filter_value = NULL,
                                     summary_func = mean, correlation = TRUE, ...) {
 
@@ -40,3 +63,6 @@ summarise.dublinairport <- function(object, filter_var = NULL, filter_value = NU
 
   return(output)
 }
+
+
+
