4a12354855af6814334524e755d6f840f5e4ea2a
AlannaCollier <alanna.collier.2022@mumail.ie>
2025-01-23 12:18:27 +0000

    error fixing for package dependencies

--- R/load_dublinairport.R
+++ R/load_dublinairport.R
@@ -24,7 +24,7 @@ load_dublinairport <- function() {
 
   if (!exists("dublin_airport", envir = .GlobalEnv)) {
     warning("The dataset 'dublin_airport' is not loaded into your environment. Loading it now.")
-    data("dublin_airport", package = "WeathR")
+    utils::data("dublin_airport", package = "WeathR")
   }
 
   if (!all(c("Mean Temperature", "Mean Daily Sunshine (hours)", "Mean Rainfall") %in% names(dublin_airport))) {
